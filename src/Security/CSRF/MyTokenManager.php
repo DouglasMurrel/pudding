62d2e7f9fe8ea8469e89fed9fc795b224bd89137
kolchin <murrel@yandex.ru>
2025-01-01 21:11:56 +0300

    CSRF

--- src/Security/CSRF/MyTokenManager.php
+++ src/Security/CSRF/MyTokenManager.php
@@ -2,10 +2,73 @@
 
 namespace App\Security\CSRF;
 
-use Symfony\Component\Security\Csrf\SameOriginCsrfTokenManager;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class MyTokenManager extends SameOriginCsrfTokenManager
 {
+final class SameOriginCsrfTokenManager implements CsrfTokenManagerInterface
+{
+    public const TOKEN_MIN_LENGTH = 24;
+
+    public const CHECK_NO_HEADER = 0;
+    public const CHECK_HEADER = 1;
+    public const CHECK_ONLY_HEADER = 2;
+
+    /**
+     * @param self::CHECK_* $checkHeader
+     * @param string[]      $tokenIds
+     */
+    public function __construct(
+        private RequestStack $requestStack,
+        private ?LoggerInterface $logger = null,
+        private ?CsrfTokenManagerInterface $fallbackCsrfTokenManager = null,
+        private array $tokenIds = [],
+        private int $checkHeader = self::CHECK_NO_HEADER,
+        private string $cookieName = 'csrf-token',
+    ) {
+        if (!$cookieName) {
+            throw new \InvalidArgumentException('The cookie name cannot be empty.');
+        }
+
+        if (!preg_match('/^[-a-zA-Z0-9_]+$/D', $cookieName)) {
+            throw new \InvalidArgumentException('The cookie name contains invalid characters.');
+        }
+
+        $this->tokenIds = array_flip($tokenIds);
+    }
+
+    public function getToken(string $tokenId): CsrfToken
+    {
+        if (!isset($this->tokenIds[$tokenId]) && $this->fallbackCsrfTokenManager) {
+            return $this->fallbackCsrfTokenManager->getToken($tokenId);
+        }
+
+        return new CsrfToken($tokenId, $this->cookieName);
+    }
+
+    public function refreshToken(string $tokenId): CsrfToken
+    {
+        if (!isset($this->tokenIds[$tokenId]) && $this->fallbackCsrfTokenManager) {
+            return $this->fallbackCsrfTokenManager->refreshToken($tokenId);
+        }
+
+        return new CsrfToken($tokenId, $this->cookieName);
+    }
+
+    public function removeToken(string $tokenId): ?string
+    {
+        if (!isset($this->tokenIds[$tokenId]) && $this->fallbackCsrfTokenManager) {
+            return $this->fallbackCsrfTokenManager->removeToken($tokenId);
+        }
+
+        return null;
+    }
+
     public function isTokenValid(CsrfToken $token): bool
     {
         if (!isset($this->tokenIds[$token->getId()]) && $this->fallbackCsrfTokenManager) {
@@ -24,7 +87,7 @@ class MyTokenManager extends SameOriginCsrfTokenManager
             return false;
         }
 
-        if (false === $isValidOrigin = $this->myIsValidOrigin($request)) {
+        if (false === $isValidOrigin = $this->isValidOrigin($request)) {
             $this->logger?->warning('CSRF validation failed: origin info doesn\'t match.');
 
             return false;
@@ -82,14 +145,80 @@ class MyTokenManager extends SameOriginCsrfTokenManager
 
         return true;
     }
-    
+
+    public function clearCookies(Request $request, Response $response): void
+    {
+        if (!$request->attributes->has($this->cookieName)) {
+            return;
+        }
+
+        $cookieName = ($request->isSecure() ? '__Host-' : '').$this->cookieName;
+
+        foreach ($request->cookies->all() as $name => $value) {
+            if ($this->cookieName === $value && str_starts_with($name, $cookieName.'_')) {
+                $response->headers->clearCookie($name, '/', null, $request->isSecure(), false, 'strict');
+            }
+        }
+    }
+
+    public function persistStrategy(Request $request): void
+    {
+        if ($request->hasSession(true) && $request->attributes->has($this->cookieName)) {
+            $request->getSession()->set($this->cookieName, $request->attributes->get($this->cookieName));
+        }
+    }
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $this->clearCookies($event->getRequest(), $event->getResponse());
+        $this->persistStrategy($event->getRequest());
+    }
+
     /**
      * @return bool|null Whether the origin is valid, null if missing
      */
-    private function myIsValidOrigin(Request $request): ?bool
+    private function isValidOrigin(Request $request): ?bool
     {
         $source = $request->headers->get('Origin') ?? $request->headers->get('Referer') ?? 'null';
 
         return 'null' === $source ? null : str_starts_with(str_replace('https://','http://',$source).'/', $request->getSchemeAndHttpHost().'/');
     }
-}
+
+    /**
+     * @return bool|null Whether the double-submit is valid, null if missing
+     */
+    private function isValidDoubleSubmit(Request $request, string $token): ?bool
+    {
+        if ($this->cookieName === $token) {
+            return null;
+        }
+
+        if ($this->checkHeader && $request->headers->get($this->cookieName, $token) !== $token) {
+            $this->logger?->warning('CSRF validation failed: wrong token found in header info.');
+
+            return false;
+        }
+
+        $cookieName = ($request->isSecure() ? '__Host-' : '').$this->cookieName;
+
+        if (self::CHECK_ONLY_HEADER === $this->checkHeader) {
+            if (!$request->headers->has($this->cookieName)) {
+                return null;
+            }
+
+            $request->cookies->set($cookieName.'_'.$token, $this->cookieName); // Ensure clearCookie() can remove any cookie filtered by a reverse-proxy
+
+            return true;
+        }
+
+        if (($request->cookies->all()[$cookieName.'_'.$token] ?? null) !== $this->cookieName && !($this->checkHeader && $request->headers->has($this->cookieName))) {
+            return null;
+        }
+
+        return true;
+    }
+} 
